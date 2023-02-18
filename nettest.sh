a3d5501e5a4e0ffdcf006de4c1d0ee85a76b2938
amol-soneji <eon747@live.com>
2023-02-18 01:01:23 -0500

    Added a required command/package in requirement list for script.

---
+++ nettest.sh
@@ -0,0 +1,8 @@
+#This shell/bash script checks network conectivity.  
+#It also displays network info and stats.  
+#This script requires the followong programs:  
+#*NMap
+#*ip
+#*ss
+#*iputils-ping
+
