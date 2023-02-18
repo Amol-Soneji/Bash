ca44ea51b5ed161e4e585b4fa69728804a32e2cc
amol-soneji <eon747@live.com>
2023-02-18 05:25:20 -0500

    Fixed command output to variable, and added on output.

--- nettest.sh
+++ nettest.sh
@@ -1,9 +1,17 @@
 #This shell/bash script checks network conectivity.  
 #It also displays network info and stats.  
-#This script requires the followong programs:  
+#This script requires the following programs:  
 #*NMap
 #*ip
 #*ss
 #*iputils-ping
 #*dnsutils
+
+
+
+pingoutput=$(ping -c 5 8.8.8.8);
+echo $(echo $pingoutput | grep -oP '.{0,5}packet loss' | tr -d ',');
+
+
+nsoutput=$(nslookup google.com);
 
