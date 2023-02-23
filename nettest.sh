abd02cb7bd1ecd8732551834e2b388c60e0bb36f
amol-soneji <eon747@live.com>
2023-02-23 02:20:12 -0500

    Made some fixes.

--- nettest.sh
+++ nettest.sh
@@ -7,6 +7,9 @@
 #*iputils-ping
 #*dnsutils
 
+#Set variable defaults.  
+pingip="8.8.8.8";
+nsname="google.com";
 
 
 pingoutput=$(ping -c 5 8.8.8.8);
@@ -14,4 +17,9 @@ echo $(echo $pingoutput | grep -oP '.{0,5}packet loss' | tr -d ',');
 
 
 nsoutput=$(nslookup google.com);
-
+if [$(echo $nsoutput | grep -io "nxdomain") == "NXDOMAIN"]
+then
+	echo "DNS TEST : FAIL"
+else
+	echo "DNS TEST : PASS"
+fi
