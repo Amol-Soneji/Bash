899524dba08fc474b9c00b7636390681b88d55f4
amol-soneji <eon747@live.com>
2023-03-05 04:23:55 -0500

    Completed available options for filtering types of logs.  Action with logs function to be done.

--- logExplorer.bash
+++ logExplorer.bash
@@ -13,8 +13,9 @@ mapfile -d $'\0' logPaths < <(find /var/log/* -type f -print0);
 keepGoing="1";
 while (($keepGoing == 1))
 do
-	printf "Enter from the following options to view a list of coresponding log files.  : \n 0-Quit \n 1-Show all log file names \n 2-Show auth files  \n 3-Show fail logs \n 4-Show login related files \n 5-Show dpkg, apt, or related installer or package maintainer logs \n 6-Show system log files \n 7-Show Xorg or X related logs \n";
+	printf "Enter from the following options to view a list of coresponding log files.  : \n 0-Quit \n 1-Show all log file names \n 2-Show auth files  \n 3-Show login related files \n 4-Show dpkg, apt, or related installer or package maintainer logs \n 5-Show OS log files \n 6-Show user log files \n 7-Show Xorg or X related logs \n";
 	read userInput;
+	declare -a argumentArray;
 	case $userInput in
 	0)
 		keepGoing=0;
@@ -28,7 +29,6 @@ do
 		logFileActions "${logPaths[@]}";
 	;;
 	2)
-		declare -a argumentArray;
 		for aLogPath in "${logPaths[@]}"
 		do
 			if [[ $(echo $aLogPath | grep -io "auth") == "auth" ]]
@@ -40,22 +40,9 @@ do
 		logFileActions "${argumentArray[@]}";
 	;;
 	3)
-		declare -a argumentArray;
 		for aLogPath in "${logPaths[@]}"
 		do
-			if [[ $(echo $aLogPath | grep -io "faillog") == "faillog" ]]
-			then
-				echo "$(basename $aLogPath)";
-				argumentArray+=$aLogPath;
-			fi
-		done
-		logFileActions "${argumentArray[@]}";
-	;;
-	4)
-		declare -a argumentArray;
-		for aLogPath in "${logPaths[@]}"
-		do
-			if [[ $(echo $logPath | grep -io "auth") == "auth" || $(echo $logPath | grep -io "wtmp") == "wtmp" || $(echo $logPath | grep -io "lastlog") == "lastlog" ]]
+			if [[ $(echo $logPath | grep -io "auth") == "auth" || $(echo $logPath | grep -io "wtmp") == "wtmp" || $(echo $logPath | grep -io "lastlog") == "lastlog" || $(echo $logPath | grep -io "faillog") == "faillog" ]]
 			then
 				echo $(basename $aLogPath);
 				argumentArray+=$aLogPath;
@@ -63,8 +50,7 @@ do
 		done
 		logFileActions "${argumentArray[@]}";
 	;;
-	5)
-		declare -a argumentArray;
+	4)
 		for aLogPath in "${logPaths[@]}"
 		do
 			if [[ $(echo $logPath | grep -io "apt") == "apt" || $(echo $logPath | grep -io "dpkg") == "dpkg" || $(echo $logPath | grep -io "installer") == "installer" ]]
@@ -75,11 +61,38 @@ do
 		done
 		logFileActions "${argumentArray[@]}";
 	;;
+	5)
+		for aLogPath in "${logpaths[@]}"
+		do
+			if [[ $(echo $logPath | grep -io "kern") == "kern" || $(echo $logPath | grep -io "syslog") == "syslog" ]]
+			then
+				echo $(basename $aLogPath);
+				argumentArray+=$aLogPath;
+			fi
+		done
+		logFileActions "${argumentArray[@]}";
+	;;
 	6)
-		#To do
+		for aLogPath in "${logpaths[@]}"
+		do
+			if [[ $(echo $logPath | grep -io "user") == "user" ]]
+			then
+				echo $(basename $aLogPath);
+				argumentArray+=$aLogPath;
+			fi
+		done
+		logFileActions "${argumentArray[@]}";
 	;;
 	7)
-		#To do
+		for aLogPath in "${logPaths[@]}"
+		do
+			if [[ $(echo $aLogPath | grep -io "lxdm") == "lxdm" || $(echo $aLogPath | grep -io "Xorg") == "Xorg" ]]
+			then
+				echo $(basename $aLogPath);
+				argumentArray+=$aLogPath;
+			fi
+		done
+		logActions "${argumentArray[@]}";
 	;;
 	*)
 		echo "Invalid input for prompt.  Please try again.  ";
